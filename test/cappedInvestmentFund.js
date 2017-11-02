ab4e46197533297fd9b9374762318ea9d8ba382d
Jose Ospina <pepo.ospina@gmail.com>
2017-11-02 20:32:10 +0100

    seconf spent test failing

--- test/cappedInvestmentFund.js
+++ test/cappedInvestmentFund.js
@@ -1,14 +1,15 @@
 var CappedInvestmentFund = artifacts.require("./CappedInvestmentFund.sol");
 
-const getSortedOffers = function (instance) {
-  var sortedOffers = [];
+const getSortedElements = function (getLowestKeyMethod, getElementAtKeyMethod) {
+  var array = [];
   return new Promise(function(resolve, reject) {
-    instance.getLowestInvestmentOfferKey.call().then(
+    getLowestKeyMethod.call().then(
       function(key) {
         if (key > 0) {
-          getSortedOffersRec(instance, key, sortedOffers).then(
-            function (sortedOffers) {
-              resolve(sortedOffers);
+          // console.log('first key: ' + key)
+          getSortedElementsRec(getElementAtKeyMethod, key, array).then(
+            function (array) {
+              resolve(array);
             })
         } else {
           resolve([]);
@@ -17,71 +18,27 @@ const getSortedOffers = function (instance) {
   });
 }
 
-const getSortedUsed = function (instance) {
-  var sortedUsed = [];
-
-  return new Promise(
-
-  function(resolve, reject) {
-    instance.getLowestInvestmentUsedKey.call().then(
-      function(key) {
-        if (key > 0) {
-          getSortedUsedRec(instance, key, sortedUsed).then(
-            function (sortedUsed) {
-              resolve(sortedUsed);
-            })
-        } else {
-          resolve([]);
-        }
-      });
-  });
-}
-
-const getSortedOffersRec = function (instance, key, sortedOffers) {
+const getSortedElementsRec = function (getElementAtKeyMethod, key, array) {
   return new Promise(function(resolve, reject) {
-    instance.getInvestmentOfferDataAtKey.call(key).then(
+    getElementAtKeyMethod.call(key).then(
       function (res) {
 
-        sortedOffers.push({
-          investor: res[0],
-          amount: res[1],
-          multiplier_micro: res[2],
-          used: res[3],
-          paid: res[4]
-        });
+        var element = {
+            investor: res[0],
+            amount: res[1],
+            multiplier_micro: res[2],
+            used: res[3],
+            paid: res[4]
+          }
+        array.push(element);
 
         var nextKey = res[5];
         if (nextKey != 0) {
           /* recursive call filling sortedOffers
           until the end of the list */
-          resolve(getSortedOffersRec(instance, nextKey, sortedOffers));
+          resolve(getSortedElementsRec(getElementAtKeyMethod, nextKey, array));
         } else {
-          resolve(sortedOffers);
-        }
-      });
-    });
-};
-
-const getSortedUsedRec = function (instance, key, sortedUsed) {
-  return new Promise(function(resolve, reject) {
-    instance.getInvestmentUsedDataAtKey.call(key).then(
-      function (res) {
-
-        sortedUsed.push({
-          investor: res[0],
-          amount: res[1],
-          multiplier_micro: res[2],
-          used: res[3],
-          paid: res[4]
-        });
-
-        var nextKey = res[5];
-        if (nextKey != 0) {
-          /* recursive call filling sortedOffers
-          until the end of the list */
-          resolve(getSortedUsedRec(instance, nextKey, sortedUsed));
-        } else {
-          resolve(sortedUsed);
+          resolve(array);
         }
       });
     });
@@ -91,11 +48,15 @@ contract('CappedInvestmentFund', function(accounts) {
 
   var investmentFund;
 
-  it ("should add a investment in order", function() {
+  it ("should add a investment in order",
+
+  function() {
 
     var orderedInvestments = [];
 
-    return CappedInvestmentFund.deployed().then(
+    return CappedInvestmentFund.deployed()
+    .then(
+
     function(instance) {
       investmentFund = instance;
       console.log('contract address: ' + investmentFund.address);
@@ -119,50 +80,67 @@ contract('CappedInvestmentFund', function(accounts) {
     }).then(
 
     function(txn) {
+      return getSortedElements(investmentFund.getLowestInvestmentOfferKey, investmentFund.getInvestmentOfferDataAtKey);
+    }).then(
 
-      getSortedOffers(investmentFund).then(function(sortedOffers) {
-        /* check the order is correct */
-        // console.log(sortedOffers);
+    function(sortedOffers) {
+      /* check the order is correct */
+      // console.log(sortedOffers);
 
-        assert.equal(sortedOffers[0].investor, accounts[2], "investment address wrong");
-        assert.equal(sortedOffers[0].amount, web3.toWei(1.3, 'ether'), "investment amount wrong");
-        assert.equal(sortedOffers[0].multiplier_micro, 900000, "investment multiplier wrong");
+      assert.equal(sortedOffers[0].investor, accounts[2], "investment address wrong");
+      assert.equal(sortedOffers[0].amount, web3.toWei(1.3, 'ether'), "investment amount wrong");
+      assert.equal(sortedOffers[0].multiplier_micro, 900000, "investment multiplier wrong");
 
-        assert.equal(sortedOffers[1].investor, accounts[0], "investment address wrong");
-        assert.equal(sortedOffers[1].amount, web3.toWei(1.1, 'ether'), "investment amount wrong");
-        assert.equal(sortedOffers[1].multiplier_micro, 1100000, "investment multiplier wrong");
+      assert.equal(sortedOffers[1].investor, accounts[0], "investment address wrong");
+      assert.equal(sortedOffers[1].amount, web3.toWei(1.1, 'ether'), "investment amount wrong");
+      assert.equal(sortedOffers[1].multiplier_micro, 1100000, "investment multiplier wrong");
 
-        assert.equal(sortedOffers[2].investor, accounts[1], "investment address wrong");
-        assert.equal(sortedOffers[2].amount, web3.toWei(1.2, 'ether'), "investment amount wrong");
-        assert.equal(sortedOffers[2].multiplier_micro, 1200000, "investment multiplier wrong");
+      assert.equal(sortedOffers[2].investor, accounts[1], "investment address wrong");
+      assert.equal(sortedOffers[2].amount, web3.toWei(1.2, 'ether'), "investment amount wrong");
+      assert.equal(sortedOffers[2].multiplier_micro, 1200000, "investment multiplier wrong");
 
-        assert.equal(sortedOffers[3].investor, accounts[3], "investment address wrong");
-        assert.equal(sortedOffers[3].amount, web3.toWei(1.4, 'ether'), "investment amount wrong");
-        assert.equal(sortedOffers[3].multiplier_micro, 1200000, "investment multiplier wrong");
+      assert.equal(sortedOffers[3].investor, accounts[3], "investment address wrong");
+      assert.equal(sortedOffers[3].amount, web3.toWei(1.4, 'ether'), "investment amount wrong");
+      assert.equal(sortedOffers[3].multiplier_micro, 1200000, "investment multiplier wrong");
 
-        assert.equal(sortedOffers[4].investor, accounts[4], "investment address wrong");
-        assert.equal(sortedOffers[4].amount, web3.toWei(1.5, 'ether'), "investment amount wrong");
-        assert.equal(sortedOffers[4].multiplier_micro, 1300000, "investment multiplier wrong");
-      })
+      assert.equal(sortedOffers[4].investor, accounts[4], "investment address wrong");
+      assert.equal(sortedOffers[4].amount, web3.toWei(1.5, 'ether'), "investment amount wrong");
+      assert.equal(sortedOffers[4].multiplier_micro, 1300000, "investment multiplier wrong");
 
+      return investmentFund.spend(web3.toWei(1.1), { from: accounts[0] });
+
+    }).then(
+
+    function(txn) {
+      return getSortedElements(investmentFund.getLowestInvestmentUsedKey, investmentFund.getInvestmentUsedDataAtKey);
+    }).then(
+
+    function(usedOffers) {
+      // console.dir(usedOffers)
+      assert.equal(usedOffers[0].used, web3.toWei(1.1, 'ether'), "didn't used partially one offer");
+
+      console.log('spending again')
+      return investmentFund.spend(web3.toWei(0.5), { from: accounts[0] });
+    }).then(
+
+    function(txn) {
+      console.log('spent again')
+      return getSortedElements(investmentFund.getLowestInvestmentUsedKey, investmentFund.getInvestmentUsedDataAtKey);
+    }).then(
+
+    function(usedOffers) {
+      console.dir(usedOffers)
     });
-
   });
 
-  // it ("should block spend to not owner", function() {
-  //   return investmentFund.spend(web3.toWei(1.1), { from: accounts[1] }).then(
+  // it ("should spend investments in order", function() {
+  //   return investmentFund.spend(web3.toWei(1.1), { from: accounts[0] }).then(
   //   function (txn) {
+  //
+  //     getSortedElements(investmentFund.getLowestInvestmentUsedKey, investmentFund.getInvestmentUsedDataAtKey)
+  //     .then(function(usedOffers) {
+  //       console.log('used offers:' + usedOffers)
+  //     });
   //   });
   // });
-
-  it ("should spend investments in order", function() {
-    return investmentFund.spend(web3.toWei(1.1), { from: accounts[0] }).then(
-    function (txn) {
-      console.log('spend block: ' + txn.receipt.blockNumber);
-      console.log('spend txn: ' + txn.tx);
-      getSortedUsed(investmentFund).then(function(usedOffers) {
-        console.log('used offers:' + usedOffers)
-      });
-    });
-  });
 });

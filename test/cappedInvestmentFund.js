92dc77e3b290326b15c63cd9f9a9f2523ec40263
Jose Ospina <pepo.ospina@gmail.com>
2017-11-02 12:13:41 +0100

    test

--- test/cappedInvestmentFund.js
+++ test/cappedInvestmentFund.js
@@ -1,5 +1,28 @@
 var CappedInvestmentFund = artifacts.require("./CappedInvestmentFund.sol");
 
+const getSortedOffers = function (instance, key, sortedOffers) {
+  return new Promise(function(resolve, reject) {
+    instance.getInvestmentOfferDataAtKey.call(key).then(
+      function (res) {
+
+        sortedOffers.push({
+          investor: res[0],
+          amount: res[1],
+          multiplier_micro: res[2]
+        });
+
+        var nextKey = res[3];
+        if (nextKey != 0) {
+          /* recursive call filling sortedOffers
+          until the end of the list */
+          resolve(getSortedOffers(instance, nextKey, sortedOffers));
+        } else {
+          resolve(sortedOffers);
+        }
+      });
+    });
+  };
+
 contract('CappedInvestmentFund', function(accounts) {
 
   var investmentFund;
@@ -36,62 +59,43 @@ contract('CappedInvestmentFund', function(accounts) {
 
     function(key) {
 
-      const getOfferAtKey = function (thisKey) {
-        return investmentFund.getInvestmentOfferDataAtKey.call(thisKey).then(
-          function (res) {
+      console.log("key: " + key);
 
-            orderedInvestments.push({
-              investor: res[0],
-              amount: res[1],
-              multiplier_micro: res[2]
-            });
+      getSortedOffers(investmentFund, key, []).then(function(sortedOffers) {
+        /* check the order is correct */
+        console.log(sortedOffers);
 
-            var nextKey = res[3];
-            if (nextKey != 0) {
-              /* recursive call filling orderedInvestments
-              until the end of the list */
-              getOfferAtKey(nextKey);
+        assert.equal(sortedOffers[0].investor, accounts[2], "investment address wrong");
+        assert.equal(sortedOffers[0].amount, web3.toWei(1.3, 'ether'), "investment amount wrong");
+        assert.equal(sortedOffers[0].multiplier_micro, 900000, "investment multiplier wrong");
 
-            } else {
-              /* at this point all the list has been run in order */
+        assert.equal(sortedOffers[1].investor, accounts[0], "investment address wrong");
+        assert.equal(sortedOffers[1].amount, web3.toWei(1.1, 'ether'), "investment amount wrong");
+        assert.equal(sortedOffers[1].multiplier_micro, 1100000, "investment multiplier wrong");
 
-              /* check the order is correct */
-              assert.equal(orderedInvestments[0].investor, accounts[2], "investment address wrong");
-              assert.equal(orderedInvestments[0].amount, web3.toWei(1.3, 'ether'), "investment amount wrong");
-              assert.equal(orderedInvestments[0].multiplier_micro, 900000, "investment multiplier wrong");
+        assert.equal(sortedOffers[2].investor, accounts[1], "investment address wrong");
+        assert.equal(sortedOffers[2].amount, web3.toWei(1.2, 'ether'), "investment amount wrong");
+        assert.equal(sortedOffers[2].multiplier_micro, 1200000, "investment multiplier wrong");
 
-              assert.equal(orderedInvestments[1].investor, accounts[0], "investment address wrong");
-              assert.equal(orderedInvestments[1].amount, web3.toWei(1.1, 'ether'), "investment amount wrong");
-              assert.equal(orderedInvestments[1].multiplier_micro, 1100000, "investment multiplier wrong");
+        assert.equal(sortedOffers[3].investor, accounts[3], "investment address wrong");
+        assert.equal(sortedOffers[3].amount, web3.toWei(1.4, 'ether'), "investment amount wrong");
+        assert.equal(sortedOffers[3].multiplier_micro, 1200000, "investment multiplier wrong");
 
-              assert.equal(orderedInvestments[2].investor, accounts[1], "investment address wrong");
-              assert.equal(orderedInvestments[2].amount, web3.toWei(1.2, 'ether'), "investment amount wrong");
-              assert.equal(orderedInvestments[2].multiplier_micro, 1200000, "investment multiplier wrong");
+        assert.equal(sortedOffers[4].investor, accounts[4], "investment address wrong");
+        assert.equal(sortedOffers[4].amount, web3.toWei(1.5, 'ether'), "investment amount wrong");
+        assert.equal(sortedOffers[4].multiplier_micro, 1300000, "investment multiplier wrong");
+      })
 
-              assert.equal(orderedInvestments[3].investor, accounts[3], "investment address wrong");
-              assert.equal(orderedInvestments[3].amount, web3.toWei(1.4, 'ether'), "investment amount wrong");
-              assert.equal(orderedInvestments[3].multiplier_micro, 1200000, "investment multiplier wrong");
-
-              assert.equal(orderedInvestments[4].investor, accounts[4], "investment address wrong");
-              assert.equal(orderedInvestments[4].amount, web3.toWei(1.5, 'ether'), "investment amount wrong");
-              assert.equal(orderedInvestments[4].multiplier_micro, 1300000, "investment multiplier wrong");
-
-            }
-          }
-        );
-      }
-
-      return getOfferAtKey(key);
     });
 
   });
 
-  it ("should spend investments in order", function() {
-    return investmentFund.spend(web3.toWei(1.1), { from: accounts[0] }).then(
-
-    function (txn) {
-      console.log(txn)
-    });
-
-  });
+  // it ("should spend investments in order", function() {
+  //   return investmentFund.spend(web3.toWei(1.1), { from: accounts[0] }).then(
+  //
+  //   function (txn) {
+  //     console.log(txn)
+  //   });
+  //
+  // });
 });
